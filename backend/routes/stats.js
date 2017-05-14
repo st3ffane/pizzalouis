7c7b9e9f89aa61304f4201da12d818363a2c2d25
st3ffane <steph.ponteins@gmail.com>
2017-05-14 22:45:53 +0200

    statistiques preparation

--- backend/routes/stats.js
+++ backend/routes/stats.js
@@ -19,13 +19,15 @@ router.get('/', db.getTotalCommandesM,
                                 icon:"fa-shopping-cart",
                                 label:"Commandes passées depuis l'application",
                                 count:req._total_count,
-                                color:"panel-primary"
+                                color:"panel-primary",
+                                link:"/admin/stats/ventes"
                             },
                         bestsell:{
                             icon:"fa-trophy",
                             label:"Meilleur vente "+req._best_sell[0].sum,
                             count:req._best_sell[0].nom,
-                            color:"panel-green"
+                            color:"panel-green",
+                            link:"/admin/stats/pizzas"
                         },
                         beststar:{
                             icon:"fa-star",
@@ -34,19 +36,22 @@ router.get('/', db.getTotalCommandesM,
                                                         { minimumFractionDigits: 2 }
                                                     )),
                             count:req._best_star[0].nom,
-                            color:"panel-yellow"
+                            color:"panel-yellow",
+                            link:"/admin/stats/notes"
                         },
                         ingredient_sell:{
                             icon:"fa-puzzle-piece",
                                 label:"Ingrédient le plus vendu",
                                 count:req._ingredient_sell[0].nom,
-                                color:"panel-red"
+                                color:"panel-red",
+                                link:"/admin/stats/ingredients"
                         } ,
                         base_sell:{
                             icon:"fa-tag",
                                 label:"Base la plus vendue",
                                 count:req._base_sell[0].nom,
-                                color:"panel-primary"
+                                color:"panel-primary",
+                                link:"/admin/stats/ingredients#bases"
                         } ,
                         sell_datas:{
                             datas:req._sell_datas
@@ -57,4 +62,45 @@ router.get('/', db.getTotalCommandesM,
                     });
 });
 
+//les vues particulieres
+router.get("/ventes",db.getSellDatas, function(req,res,next){
+    res.render('details/ventes',{
+        title:"Evolution des ventes",
+        datas:{
+
+        }
+    })
+});
+router.get("/pizzas",db.getBestSellsM, function(req,res,next){
+    res.render('details/pizzas',{
+        title:"Evolution des ventes par pizzas!",
+        datas:{
+            
+        }
+    })
+});
+router.get("/ingredients",db.getIngredientsSells, db.getBaseSells, function(req,res,next){
+    res.render('details/ingredients',{
+        title:"Consommation des ingrédients/bases",
+        datas:{
+            
+        }
+    })
+});
+router.get("/notes",db.getBestStart, function(req,res,next){
+    res.render('details/notes',{
+        title:"Les pizzas préférées de vos clients",
+        datas:{
+            
+        }
+    })
+});
+router.get("/geo",db.getGeolocCommands, function(req,res,next){
+    res.render('details/geo',{
+        title:"Répartition géographique des commandes",
+        datas:{
+            
+        }
+    })
+})
 module.exports = router;
