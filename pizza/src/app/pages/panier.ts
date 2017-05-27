149aaeb447c409d64d5295238e68431fce49ddb5
st3ffane <steph.ponteins@gmail.com>
2017-05-27 16:18:03 +0200

    paypal process

--- pizza/src/app/pages/panier.ts
+++ pizza/src/app/pages/panier.ts
@@ -17,14 +17,20 @@ export default class PanierComponent{
     retrait="17:30:00";//heure de retrait 
     date_error = "";
     message:string = "";//message complémentaire
-
+    nonce:string = null;//autorisation de paiements paypal
 
     show_checkout:boolean = false;
     instance:any;
+    is_processing:boolean = false;
+    display_command:boolean = true;
+    error:string = "";
+
+
     
     constructor(private _panier:PanierProvider, private _ws:WSProvider, private _router:Router){}
     ngOnInit(){
         window.scrollTo(0,0);
+        this.instance = null;
         this._panier.getCardDetails().then(dt=>{
             this.card = dt;  
         });
@@ -32,6 +38,7 @@ export default class PanierComponent{
 
     processForm(){
         this.date_error = "";
+        this.error = "";
 
         if(this.retrait < "17:30" || this.retrait > "21:30"){
             this.date_error = "Les horaires d'ouvertures sont compris entre 17h30 et 21h30.";
@@ -40,40 +47,39 @@ export default class PanierComponent{
         }
 
         //lance paypal????
+        this.is_processing =true;
+        this._ws.postCommand("fake-valid-nonce", this.card).then( dt=>{
+                            //tout est OK, file a la derniere page
+                            
+                            this._router.navigate(['/complete']);
+                        }).catch(err=>{
+                            this.error = err;
+                            this.is_processing =false;
+                        });
         
-        this.instance.requestPaymentMethod((requestPaymentMethodErr, payload)=> {
-          // Submit payload.nonce to your server
-          console.log(payload.nonce);
-          this._ws.postCommand("fake-valid-nonce", this.card).then( dt=>{
-                    //tout est OK, file a la derniere page
-                    this._router.navigate(['/complete']);
-                }).catch(err=>{
-                    console.log(err);
-                });
-
-
-        });
       
         
     }
     openCheckout(){
         if(this.card.total == 0) return;
-
+        this.is_processing = true;
         //recupere un token du serveur 
         this._ws.getClientToken().then( dt=>{
             
             braintree.dropin.create({
             authorization: dt.token,
-            selector: '#dropin-container'
+            selector: '#dropin-container',
+            locale:"fr-FR"
             }, (err, instance) => {
                 if(err){
                     console.log("Erreur recuperation du checkout....")
                     console.log(err);
-                    this.show_checkout = false;
                     return;
                 }
 
                 this.instance = instance;
+                this.is_processing = false;
+                /**/
                 //
                 /*
                 //balance le resultat au serveur
@@ -89,5 +95,24 @@ export default class PanierComponent{
         
       
        this.show_checkout = true;
+    }
+    requestPayload(){
+        if(!this.instance) return;
+        this.is_processing = true;
+        this.instance.requestPaymentMethod((requestPaymentMethodErr, payload)=> {
+                    // Submit payload.nonce to your server
+                    if(requestPaymentMethodErr){
+                        //affiche un message
+                        
+                        this.is_processing = false;
+
+                        return;
+                    } else {
+                        console.log(payload.nonce);
+                        this.nonce = payload.nonce;
+                    }                    
+                    this.display_command = false;//plus de bouton
+                    this.is_processing = false;
+                });
     }
 }
